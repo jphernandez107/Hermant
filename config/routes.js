29fcbb419560b4fc90f84b4ea9f3abc828810c90
yoojuaan <yoojuaan@gmail.com>
2019-03-23 20:52:37 -0300

    Se agrego una nueva vista, homepage.ejs en la cual se va a mostrar la pagina principal del sistema. Tambien se agrego al archivo routes.js.

--- config/routes.js
+++ config/routes.js
@@ -19,6 +19,9 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
+  '/homepage': {
+    view: 'pages/homepage',
+  },
   '/': {
     view: 'pages/signin',
   },
