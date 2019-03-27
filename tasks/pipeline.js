e35a1b63ccc1598bf5cdd16b5f2eb608c673c9df
yoojuaan <yoojuaan@gmail.com>
2019-03-27 20:32:01 -0300

    Se modifico el layout para mostrar siempre un sidebar con ciertas opciones. Tambien se agregaron y eliminaron archivos CSS y JS necesarios e innecesarios para el funcionamiento del sidebar responsive.

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -63,6 +63,9 @@ var jsFilesToInject = [
 
   // Load `sails.io` before everything else.
   'dependencies/sails.io.js',
+  //'js/Homepage/jquery.min.js',
+  /*'js/Homepage/jquery.slim.min.js',
+  'js/Homepage/chartist.min.js',*/
 
   // Bring in `.js` files for any other client-side JavaScript dependencies.
   // (e.g. Lodash, Vue.js, jQuery, Bootstrap, Ember, Angular, etc.)
