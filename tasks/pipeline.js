f20d3bf3d7901306f086b211cace331a2193c51a
yoojuaan <yoojuaan@gmail.com>
2019-03-31 19:24:17 -0300

    Se modificaron las view de signin y signup para mostrar su propio estilo. Tambien se modifico el pipeline para que no busque en la ruta el archivo css correspondiente a estas views.

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -39,7 +39,8 @@ var cssFilesToInject = [
   // All of the rest of your custom `.css` files will be injected here,
   // in no particular order.  To customize the ordering, add additional
   // items here, _above_ this one.
-  'styles/**/*.css'
+  'styles/**/*.css',
+  '!styles/local/*.css'
 ];
 
 
