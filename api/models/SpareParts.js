2d41586c735390962405ac117970b1bca0ba0515
Matias Insaurralde <matiasinsaurralde@Matiass-MacBook-Air.local>
2019-04-02 19:54:43 -0300

    se agregó el modelo de repuestos con sus caracteristicas.

---
+++ api/models/SpareParts.js
@@ -0,0 +1,35 @@
+
+
+module.exports = {
+    attributes: {
+
+        internalCode:{
+            type:'string',
+        },
+
+        externalCode:{
+            type:'string',
+        },
+
+        type:{
+            type:'string',
+        },
+
+        application:{
+            type:'string',
+        },
+
+        brand:{
+            type:'string',
+        },
+
+        model:{
+            type:'string',
+        },
+
+        stock:{
+            type:'number',
+        },
+
+    }
+}
