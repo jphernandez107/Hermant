71d665f4a9e3756b1a43c4585efe3d2789fdbcb5
yoojuaan <yoojuaan@gmail.com>
2019-04-20 12:28:52 -0300

    API para el manejo de los datos de las frecuencias de mantenimiento

---
+++ api/models/MaintenanceFrequency.js
@@ -0,0 +1,43 @@
+/**
+ * MaintenanceFrequency.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    type:{
+      type:'number',
+    },
+    frequency:{ // 250hs - 500Hs - etc
+      type:'number',
+    },
+    change:{  // En cual mantenimiento correponde cambio
+      type:'boolean'
+    },
+
+
+    lubricationSheetRow:{
+      model: 'LubricationSheetRow',
+    },
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
