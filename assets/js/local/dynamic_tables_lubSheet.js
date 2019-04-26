7d01c54defb88ae2c8d34144cc5c5dd332e668e7
yoojuaan <yoojuaan@gmail.com>
2019-04-26 11:51:56 -0300

    Se soluciono problema de javascript (JQuery)

--- assets/js/local/dynamic_tables_lubSheet.js
+++ assets/js/local/dynamic_tables_lubSheet.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var tables = document.getElementsByClassName('table-sheet');
-
-  for(table of tables){
+  console.log("Tabla con " + 'Hola' + " filas.");
+  for(var table of tables){
     console.log("Tabla con " + table.rows.length + " filas.");
     if(table.rows.length == 2){
       //table.parentNode.removeChild(table);
