4055c0207bcf95c1745c1c47fa5b4bc3a122611d
yoojuaan <yoojuaan@gmail.com>
2019-05-09 13:45:43 -0300

    Se modifico el codigo para guardar los eventos en el nuevo modelo

--- api/controllers/EquipmentUseHourController.js
+++ api/controllers/EquipmentUseHourController.js
@@ -24,6 +24,8 @@ module.exports = {
       await Equipment.updateOne({id:equipmentId}).set({totalHours:newTotalHours, partialHours:newPartialHours});
       await EquipmentUseHour.create({date, hours:time, equipment:equipmentId});
 
+      update_calendar_events_list();
+
       return res.redirect('/equipmentusehour/list');
     }else{
       return res.redirect('/equipmentusehour/list');
@@ -43,166 +45,169 @@ module.exports = {
     }
   },
 
-  calendar_events_list: async function(req,res){
-
-    function stringToDate(_date){
-      var _delimiter = "/";
-      var formatLowerCase=String("dd/mm/yyyy");
-      var formatItems=formatLowerCase.split(_delimiter);
-      var dateItems=_date.split(_delimiter);
-      var monthIndex=formatItems.indexOf("mm");
-      var dayIndex=formatItems.indexOf("dd");
-      var yearIndex=formatItems.indexOf("yyyy");
-      var month=parseInt(dateItems[monthIndex]);
-      month-=1;
-      var formatedDate = new Date(dateItems[yearIndex],month,dateItems[dayIndex]);
-      return formatedDate;
-    }
-
-    function distinct(value, index, self) {
-        return self.indexOf(value) === index;
-    }
-
-    var today = new Date();
-    var monthAgo = new Date();
-    monthAgo.setDate(today.getDate() - 30);
-    monthAgo.setHours(0,0,0,0)
-
-    //var useHours = await EquipmentUseHour.find();
-    var equipments = await Equipment.find().populate('equipmentUseHours');
-
-    var events = '[';
-
-    for(equipment of equipments){
-      var dates = [];
-      var totalHours = 0;
-      var avg = 7;
-      //console.log(equipment.equipmentUseHours.length);
-      if(equipment.equipmentUseHours.length > 0){
-        for(useHour of equipment.equipmentUseHours){
-          var date = stringToDate(useHour.date);
-          if(date.getTime() >= monthAgo.getTime()){
-            dates.push(useHour.date);
-            totalHours += Number(useHour.hours);
-            //console.log("Toy dentro de los 3 dias");
-          }
-          //console.log(useHour.date);
-        }
-      }
-      // console.log('El equipo ' + equipment.id +' tiene ' + totalHours + 'Hs de uso en ' + dates.filter(distinct).length + ' dias');
-      // console.log(dates.filter(distinct));
-      if(dates.filter(distinct).length > 0){
-        avg = Math.round(totalHours / dates.filter(distinct).length * 100) / 100;
-      }
-
-      // Tengo que conseguir los valores de las frecuencias de mantenimientos de cada equipo.
-      if(equipment.lubricationSheet != null){
-        var sheetRows = await LubricationSheetRow.find({lubricationSheet:equipment.lubricationSheet});
-        var uniqueMaintFreqs = await MaintenanceFrequency.find({lubricationSheetRow:sheetRows[0].id});
-        var uniqueFreqs = [];
-        for(uMF of uniqueMaintFreqs){
-          uniqueFreqs.push(uMF.frequency);
-        }
-        //console.log(uniqueFreqs)
-
-        var partialHours2 = equipment.partialHours;
-        var nextMaintenance = 0;
-        var hoursToNextMaintenance = 0;
-        var tmpPartialHours = 0;
-        var partialHours = 0;
-        for(var j=0; j<10; j++){
-          partialHours = partialHours2;
-          for(var i = uniqueFreqs.length-1; i>=1 ;i--){
-            if(partialHours >= uniqueFreqs[uniqueFreqs.length-1]){
-              partialHours -= uniqueFreqs[uniqueFreqs.length-1];
-            }
-            if(partialHours > uniqueFreqs[i]){
-              partialHours -= uniqueFreqs[i];
-            }else{
-              nextMaintenance = uniqueFreqs[i];
-              if(i < uniqueFreqs.length - 1){
-                //hoursToNextMaintenance -= uniqueFreqs[i+1];
-              }
-            }
-          }
-          //console.log(tmpPartialHours + 'Hs, nextMaintenance -> ' + nextMaintenance + 'Hs')
-          hoursToNextMaintenance = uniqueFreqs[1] + partialHours2 - equipment.partialHours*2;
-
-          //console.log('Faltan ' + hoursToNextMaintenance + 'Hs para el prox mant');
-
-          var daysToNextMaintenance = Math.round(hoursToNextMaintenance/avg);
-
-          var dateNextMaintenance = new Date();
-          dateNextMaintenance.setDate(dateNextMaintenance.getDate() + daysToNextMaintenance);
-
-          //console.log('Faltan ' + daysToNextMaintenance + ' dias para el prox mant');
-          dateNextMaintenance.setHours(0,0,0,0);
-          var dd = String(dateNextMaintenance.getDate()).padStart(2, '0');
-          var mm = String(dateNextMaintenance.getMonth() + 1).padStart(2, '0');
-          var yyyy = dateNextMaintenance.getFullYear();
-
-          var newdate = yyyy + "-" + mm + "-" + dd;
-          //console.log(newdate);
-          events += '{"title":"' + nextMaintenance + 'Hs ' + equipment.designation + ' ' + equipment.brand + '",';
-          events += '"start":"' + newdate + '",';
-          events += '"url": "/equipment/details/' + equipment.id + '"},';
-
-
-          partialHours2 += uniqueFreqs[1];
-
-          if(nextMaintenance === uniqueFreqs[uniqueFreqs.length-1]){
-            tmpPartialHours += uniqueFreqs[uniqueFreqs.length-1];
-          }
-
-          //console.log(equipment.partialHours + 'Hs, nextMaintenance -> ' + nextMaintenance + 'Hs. El dia: ' + dateNextMaintenance);
-
-        }
-      }
-    }
-    events = events.substring(0, events.length-1);
-    events += ']';
-
-    'use strict';
-    //let data = JSON.stringify(events, null, 2);
-    //console.log(data);
-    //const fs = require('fs');
-    //fs.writeFileSync('./assets/json/calendar_events.json', events);
-     const Fs = require('fs-extra');
-     await Fs.writeFile('./assets/json/calendar_events.json', events);
-    // //const json = JSON.stringify(data, null, 2);
-    //
-    // try {
-    //   await Fs.writeFile('./assets/json/calendar_events.json', events);
-    //   console.log('Saved data to file.')
-    // } catch (error) {
-    //   console.error(error)
-    // }
-
+  show_calendar: async function(req,res){
+    //calendar_events_list();
     return res.view('pages/calendar/calendar');
-
   },
 
   readJsonFile: async function (req, res) {
-
-    'use strict';
-    const fs = require('fs');
-    let rawdata = fs.readFileSync('./assets/json/calendar_events.json');
-    let events = JSON.parse(rawdata);
-    //console.log(student[0].title);
+    //Busco el archivo JSON con los datos de los eventos y lo devuelvo.
+    //'use strict';
+    //const fs = require('fs');
+    //let rawdata = fs.readFileSync('./assets/json/calendar_events.json');
+    //let events = JSON.parse(rawdata);
+    let ev = await Events.findOne({id:1});
+    let events = ev.events;
     if(events){
       return res.json(events)
     }
 
   },
 
-  writeJsonFile: async function (req, res) {
-
-    'use strict';
-    const fs = require('fs');
-    let events = '[{"title": "Mantenimiento Maquina 1", "start": "2019-05-02 00:00:00"},{"title": "Mantenimiento Maquina 2", "start": "2019-05-03 00:00:00"}]';
-    let data = JSON.parse(events);
-    //fs.writeFileSync('./assets/json/calendar_events.json', data);
-
-  },
-
 };
+
+async function update_calendar_events_list(){
+  /*
+    Calculo el promedio de horas de uso por dia que tiene un equipo en un mes
+    hacemos una prediccion de los proximos mantenimientos de cada equipo
+    en base al promedio de uso y guardo los datos en el JSON de eventos
+  */
+
+  function stringToDate(_date){
+    var _delimiter = "/";
+    var formatLowerCase=String("dd/mm/yyyy");
+    var formatItems=formatLowerCase.split(_delimiter);
+    var dateItems=_date.split(_delimiter);
+    var monthIndex=formatItems.indexOf("mm");
+    var dayIndex=formatItems.indexOf("dd");
+    var yearIndex=formatItems.indexOf("yyyy");
+    var month=parseInt(dateItems[monthIndex]);
+    month-=1;
+    var formatedDate = new Date(dateItems[yearIndex],month,dateItems[dayIndex]);
+    return formatedDate;
+  }
+
+  function distinct(value, index, self) {
+      return self.indexOf(value) === index;
+  }
+
+  var today = new Date();
+  var monthAgo = new Date();
+  monthAgo.setDate(today.getDate() - 30);
+  monthAgo.setHours(0,0,0,0)
+
+  var equipments = await Equipment.find().populate('equipmentUseHours');
+
+  var events = '[';
+
+
+  // Busco todas las obras y a cada ID de obra le asigno un color aleatorio
+  var colorArray = ['#0A6633', '#F0B3A9', '#F3D3FF', '#22AA99', '#00B3E6',
+                    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+                    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+                    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+                    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+                    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+                    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+                    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+                    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+                    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+
+
+  for(equipment of equipments){
+    // Calculo el promedio de horas de uso que tiene cada equipo
+    console.log(equipment.code);
+    var dates = [];
+    var totalHours = 0;
+    var avg = 7;
+    if(equipment.equipmentUseHours.length > 0){
+      for(useHour of equipment.equipmentUseHours){
+        var date = stringToDate(useHour.date);
+        if(date.getTime() >= monthAgo.getTime()){
+          dates.push(useHour.date);
+          totalHours += Number(useHour.hours);
+        }
+      }
+    }
+    // Filtro las fechas encontradas y me quedo solo con las que no se repiten para el calculo del promedio.
+    // Esto es porque si agregaron varios objetos de horas con una misma fecha, no tome los dos objetos si
+    // no que filtre la fecha unicamente.
+    if(dates.filter(distinct).length > 0){
+      avg = Math.round(totalHours / dates.filter(distinct).length * 100) / 100;
+    }
+
+    // Tengo que conseguir los valores de las frecuencias de mantenimientos de cada equipo.
+    if(equipment.lubricationSheet != null){
+      var sheetRows = await LubricationSheetRow.find({lubricationSheet:equipment.lubricationSheet});
+      var uniqueMaintFreqs = await MaintenanceFrequency.find({lubricationSheetRow:sheetRows[0].id});
+      var uniqueFreqs = [];
+      for(uMF of uniqueMaintFreqs){
+        uniqueFreqs.push(uMF.frequency);
+      }
+
+      /*
+      Busco todos los X proximos mantenimientos de cada equipo, y los registro en JSON de eventos
+      */
+      var partialHours2 = equipment.partialHours;
+      var nextMaintenance = 0;
+      var hoursToNextMaintenance = 0;
+      var partialHours = 0;
+      for(var j=0; j<5; j++){
+        partialHours = partialHours2;
+        for(var i = uniqueFreqs.length-1; i>=1 ;i--){
+          if(partialHours >= uniqueFreqs[uniqueFreqs.length-1]){
+            partialHours -= uniqueFreqs[uniqueFreqs.length-1];
+          }
+          if(partialHours > uniqueFreqs[i]){
+            partialHours -= uniqueFreqs[i];
+          }else{
+            nextMaintenance = uniqueFreqs[i];
+          }
+        }
+
+        hoursToNextMaintenance = uniqueFreqs[1] + partialHours2 - equipment.partialHours*2;
+
+        var daysToNextMaintenance = Math.round(hoursToNextMaintenance/avg);
+
+        //Creo una fecha de hoy y le sumo los dias que faltan para el proximo manteimiento
+        var dateNextMaintenance = new Date();
+        dateNextMaintenance.setDate(dateNextMaintenance.getDate() + daysToNextMaintenance);
+
+        //Seteo las horas y minutos a 0.
+        dateNextMaintenance.setHours(0,0,0,0);
+        if(dateNextMaintenance.getDay() == 0){// Si cae domingo, lo paso para el lunes
+          dateNextMaintenance.setDate(dateNextMaintenance.getDate() + 1);
+        }else if(dateNextMaintenance.getDay() == 6){// Si cae sabado, lo adelanto para el viernes
+          dateNextMaintenance.setDate(dateNextMaintenance.getDate() - 1);
+        }
+        var dd = String(dateNextMaintenance.getDate()).padStart(2, '0');
+        var mm = String(dateNextMaintenance.getMonth() + 1).padStart(2, '0');
+        var yyyy = dateNextMaintenance.getFullYear();
+
+        var newdate = yyyy + "-" + mm + "-" + dd;
+        events += '{"title":"[' + nextMaintenance + 'Hs] Equipo ' + (equipment.code) + '",';
+        events += '"start":"' + newdate + '",';
+        if(equipment.constructionSite != null){
+          events += '"color":"' + String(colorArray[equipment.constructionSite]) + '",';
+        }
+        events += '"url": "/equipment/details/' + equipment.id + '"},';
+
+        partialHours2 += uniqueFreqs[1];
+      }
+    }
+  }
+  events = events.substring(0, events.length-1);
+  events += ']';
+
+  if(Events.count() > 0){
+    await Events.updateOne({id:1}).set({events:events.JSON.parse(events)});
+  }else{
+    console.log("creando JSON");
+    await Events.create({events:JSON.parse(events)});
+  }
+
+  // 'use strict';
+  //  const Fs = require('fs-extra');
+  //  await Fs.writeFile('./assets/json/calendar_events.json', events);
+}
