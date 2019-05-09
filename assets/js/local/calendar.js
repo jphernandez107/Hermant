fa77eb9c58f09f217b8096ff8efe25ee667bf6ba
yoojuaan <yoojuaan@gmail.com>
2019-05-09 13:46:37 -0300

    Se quito la accion al tocar un dia.

--- assets/js/local/calendar.js
+++ assets/js/local/calendar.js
@@ -21,11 +21,11 @@ $(document).ready(function() {
       }
     },
     weekends: true,
-    dayClick: function(day) {
-      var d = new Date(day + 3*60*60*1000);
-      //d.setUTCSeconds(day/1000);
-      alert('a day has been clicked!' + d);
-    },
+    // dayClick: function(day) {
+    //   var d = new Date(day + 3*60*60*1000);
+    //   //d.setUTCSeconds(day/1000);
+    //   alert('a day has been clicked!' + d);
+    // },
 
     defaultView: 'month',
     //color: '#378006',
